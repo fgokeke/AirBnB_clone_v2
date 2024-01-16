f14f8126791cb7b17d4c7f2c531bd9bc65db5c65
root <okekefeargod@gmail.com>
2024-01-16 09:48:42 -0800

    2. Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to web servers
+"""
+
+
+from fabric.api import env, put, run
+from os.path import exists
+
+
+env.hosts = ['54.237.117.225', '34.207.156.91']
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to web servers
+    Args:
+        archive_path (str): the path to the archive to be deployed
+    Returns:
+        True if all operations were successful, otherwise returns False
+    """
+    # Check if the archive_path exists
+    if not exists(archive_path):
+        return False
+
+    # Extract the file name and the name without extension
+    file_name = archive_path.split("/")[-1]
+    name_without_ext = file_name.split(".")[0]
+
+    # Define the directory paths
+    remote_tmp_path = f"/tmp/{file_name}"
+    release_dir = f"/data/web_static/releases/{name_without_ext}/"
+
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, remote_tmp_path)
+        # Uncompress the archive in the release directory on the web server
+        run(f"mkdir -p {release_dir}")
+        run(f"tar -xzf {remote_tmp_path} -C {release_dir}")
+
+        # Delete the archive from the web server
+        run(f"rm {remote_tmp_path}")
+
+        # Move content out from web_static folder to the release dir
+        run(f"mv {release_dir}web_static/* {release_dir}")
+
+        # Remove the empty directory after moving its content
+        run(f"rm -rf {release_dir}web_static")
+
+        # Delete the current symbolic link
+        run(f"rm -rf /data/web_static/current")
+
+        # Create a new symbolic link to the new release dir
+        run(f"ln -s {release_dir} /data/web_static/current")
+
+        return True
+    except Exception as e:
+        return False
