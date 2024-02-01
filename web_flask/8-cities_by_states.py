75603044cc3c8a84b36d2ae8efb3b9321f72c9fc
root <okekefeargod@gmail.com>
2024-02-01 08:01:37 -0800

    9. Cities by states 3

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """
-a script that starts a Flask web application and presents cities by states
+starts a Flask web application
 """
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display the states and cities listed by state id and name."""
+    """display the states and cities listed in alphabetical order"""
     states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the storage on teardown."""
+    """closes the storage on teardown"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
